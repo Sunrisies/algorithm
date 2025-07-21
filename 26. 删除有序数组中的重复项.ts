7160507e48ce739ba13feba9622ffaa208b54308
朱重迁 <z32664206862022@163.com>
2025-07-21 14:10:35 +0800

    feat: 添加两个去重数组的新方法
    添加了两种去重数组的新方法：
    - 使用快慢指针的方法：通过维护两个指针，一个指向当前唯一序列的末尾（slow），另一个用于遍历整个数组（fast）。当遇到新值时，扩充唯一序列，并返回新长度。
    - 倒序遍历数组的方法：从后向前遍历数组，遇到重复元素时直接删除，最后返回数组的新长度。

--- 26. 删除有序数组中的重复项.ts
+++ 26. 删除有序数组中的重复项.ts
@@ -16,8 +16,38 @@
     输出：5, nums = [0,1,2,3,4]
     解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
  */
+// 解法1：使用快慢指针
 function removeDuplicates(nums: number[]): number {
-    let k = 0;
-    let len = nums.length;
-    // for (let i = 0;i < )
+    // 数组为空直接返回 0
+    if (nums.length === 0) return 0;
+
+    // slow 指向当前唯一序列的末尾
+    let slow = 0;
+
+    // fast 负责遍历整个数组
+    for (let fast = 1; fast < nums.length; fast++) {
+        // 当遇到新值时，扩充唯一序列
+        if (nums[fast] !== nums[slow]) {
+            slow++;               // 先移动 slow，指向待写入的位置
+            nums[slow] = nums[fast]; // 把新值写到 slow 的位置
+        }
+        // 如果 nums[fast] === nums[slow]，说明重复，直接跳过
+    }
+
+    // slow 从 0 开始计数，所以长度需要 +1
+    return slow + 1;
 };
+
+// 解法2: 倒叙遍历数组，遇到重复就删除
+function removeDuplicates3(nums: number[]): number {
+    // 倒着遍历，遇到重复就删除
+    for (let i = nums.length - 1; i > 0; i--) {
+        if (nums[i] === nums[i - 1]) nums.splice(i, 1);
+    }
+    // 此时数组已修改完成，返回新长度即可
+    return nums.length;
+}
+// const result1 = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+// console.log(result1); // 2, [1, 2, _]
+const result2 = removeDuplicates([1, 2, 3, 1]);
+console.log(result2); // 2, [1, 2, _]
