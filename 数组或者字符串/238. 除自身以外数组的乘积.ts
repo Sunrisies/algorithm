e56d100fa4c5227e6a88c51834862262191b6f66
朱重迁 <z32664206862022@163.com>
2025-07-28 17:47:33 +0800

    feat: 实现数组元素乘积功能
    - 新增函数 `productExceptSelf`，用于计算数组中除自身外其余元素的乘积
    - 函数通过遍历数组并使用 `reduce` 方法计算每个元素前缀和后缀的乘积
    - 示例中包含输入 `[1, 2, 3, 4]` 的测试用例，输出 `[24,12,8,6]`
    - 示例中包含输入 `[-1, 1, 0, -3, 3]` 的测试用例，但未启用
    
    当前实现仅计算了前缀乘积，后缀乘积部分尚未实现。

---
+++ 数组或者字符串/238. 除自身以外数组的乘积.ts
@@ -0,0 +1,27 @@
+/**
+ * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+
+    题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+
+    请 不要使用除法，且在 O(n) 时间复杂度内完成此题。
+    示例 1:
+    输入: nums = [1,2,3,4]
+    输出: [24,12,8,6]
+
+    示例 2:
+    输入: nums = [-1,1,0,-3,3]
+    输出: [0,0,9,0,0]
+ */
+function productExceptSelf(nums: number[]): number[] {
+    let result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        console.log(i, '111111')
+        let chart = nums.slice(0, i).reduce((a, b) => a * b, 1);
+        console.log(chart, 'chart')
+    }
+
+    return result;
+};
+console.log(productExceptSelf([1, 2, 3, 4])); // [24,12,8,6]
+// console.log(productExceptSelf([-1, 1, 0, -3, 3])); // [0,0,9,0,0]
