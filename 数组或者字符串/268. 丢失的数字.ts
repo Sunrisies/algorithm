6090058d424170d8b74fe5fb4ce735569071d9e9
朱重迁 <z32664206862022@163.com>
2025-08-05 15:43:36 +0800

    feat: 添加查找缺失数字的功能
    在数组中查找范围 [0, n] 内缺失的数字。
    
    - 添加了一个名为 `missingNumber` 的函数，该函数接受一个数字数组 `nums` 作为参数。
    - 该函数首先获取数组的长度 `n`。
    - 如果数组长度为 1，则根据数组中的唯一数字判断缺失的数字是 0 还是 1。
    - 对数组进行排序，确保数字按升序排列。
    - 遍历排序后的数组，检查相邻数字之间的差值是否大于 1，如果是，则返回缺失的数字。
    - 如果遍历完成后没有发现缺失的数字，则返回 `n`，表示 `n` 是缺失的数字。
    - 在代码末尾添加了多个测试用例，验证 `missingNumber` 函数的正确性。

---
+++ 数组或者字符串/268. 丢失的数字.ts
@@ -0,0 +1,38 @@
+/**
+ * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+
+    示例 1：
+    输入：nums = [3,0,1]
+    输出：2
+    解释：n = 3，因为有 3 个数字，所以所有的数字都在范围 [0,3] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+
+    示例 2：
+    输入：nums = [0,1]
+    输出：2
+    解释：n = 2，因为有 2 个数字，所以所有的数字都在范围 [0,2] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+
+    示例 3：
+    输入：nums = [9,6,4,2,3,5,7,0,1]
+    输出：8
+    解释：n = 9，因为有 9 个数字，所以所有的数字都在范围 [0,9] 内。8 是丢失的数字，因为它没有出现在 nums 中。
+ */
+
+function missingNumber(nums: number[]): number {
+    // 判断一个数组是否是正常自增的
+    const n = nums.length
+    if (n === 1) return nums[0] === 0 ? 1 : 0
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i + 1] - nums[i] > 1) {
+            return nums[i] + 1
+        }
+    }
+    return n
+};
+console.log(missingNumber([3, 0, 1])); // 2
+console.log(missingNumber([0, 1])); // 2
+console.log(missingNumber([1, 2, 0])); // 3
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // 8
+console.log(missingNumber([0])); // 1
+console.log(missingNumber([1])); // 0
+console.log(missingNumber([1, 2])); // 1
